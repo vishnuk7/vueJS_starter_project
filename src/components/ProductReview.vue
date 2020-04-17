70d05027de7b5be424a765c8f0d9d736341e7ca7
viu <vishnu.kraman88@gmail.com>
2020-04-18 00:55:13 +0530

    two way binding v-model

---
+++ src/components/ProductReview.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <form>
+      <input v-model="name" type="text" autofocus placeholder="Enter your name" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ProductReview",
+  data() {
+    return {
+      name: null
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
