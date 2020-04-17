b86669064741c2ec1ee102733b1b3bb15f079d94
viu <vishnu.kraman88@gmail.com>
2020-04-17 18:40:56 +0530

    conditional rendering

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -6,6 +6,8 @@
     <div class="product-info">
       <h5>{{ product }}</h5>
       <p>{{ description }}</p>
+      <p v-if="isStock">In Stock</p>
+      <p v-else>Out of Stock</p>
     </div>
   </div>
 </template>
@@ -17,10 +19,14 @@ export default {
     this.product = "Scoks";
     this.description = "A pair of warm, fuzzy socks";
     this.img = "../assets/scoks-1.jpeg";
+    this.isStock = false;
   }
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+img {
+  width: 450px;
+}
 </style>
